584426b2aa90e92c1f342fb4aad58cc7903e7a3b
matheusjp201 <matheusjp201fjv@gmail.com>
2021-08-24 14:58:40 -0300

    Negociaciones-Estudios

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,18 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+  private negociacoes: Negociacao[] = [];
+
+  adiciona(negociacao: Negociacao) {
+    this.negociacoes.push(negociacao);
+  }
+
+  lista(): readonly Negociacao[] {
+    return this.negociacoes;
+  }
+}
+
+
+
+//Negociacao[] = Array<Negociacao>
+//ReadonlyArray<Negociacao> = readonly Negociacao[]
